619265ac22ebe97e2909e9f97a5a5f9e03724e53
Max Marcon <maxmarcon@gmx.net>
2019-07-06 15:12:19 +0200

    removed stale test

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,8 +1,5 @@
 require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
-  test "should get test" do
-    get v1_api_test_url
-    assert_response :success
-  end
+  # add tests here
 end
